1232a50dc49ef1c36e48dee81c2e7431814fe58a
MajoraDev <Majora@Dev>
2022-09-06 20:39:15 +0200

    Add project files.

---
+++ VRGreen-Troubleshooter/Helpers.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Management;
+
+namespace VRGreen_Troubleshooter
+{
+    internal class Helpers
+    {
+        public static void CheckRealTimeProtectionState()
+        {
+            try
+            {
+                RegistryKey reg = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                if (reg != null)
+                {
+                    var value = reg.OpenSubKey(@"SOFTWARE\Microsoft\Windows Defender\Real-Time Protection").GetValue("DisableRealtimeMonitoring");
+                    if (value != null)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("[RealTimeProtection]: Your Windows Defender real-time protection is off.");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[RealTimeProtection]: Your Windows Defender real-time protection is ON!");
+                        Console.ResetColor();
+                    }
+                }
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[RealTimeProtection]: Something went wrong checking for RealTimeProtectionState!");
+                Console.ResetColor();
+            }
+        }
+
+        public static void CheckForAntiVirus()
+        {
+            ManagementObjectSearcher wmiData = new ManagementObjectSearcher(@"root\SecurityCenter2", "SELECT * FROM AntiVirusProduct");
+            ManagementObjectCollection data = wmiData.Get();
+            foreach (ManagementObject virusChecker in data)
+            {
+                var virusCheckerName = virusChecker["displayName"];
+            }
+        }
+
+        public static void CheckCoreIsolationState()
+        {
+            try
+            {
+                RegistryKey reg = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                if (reg != null)
+                {
+                    var value = reg.OpenSubKey(@"Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity");
+                    if (value == null)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("[CoreIsolation]: Your Core Isolation is off.");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[CoreIsolation]: Your Core Isolation is ON!");
+                        Console.ResetColor();
+                    }
+                }
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[CoreIsolation]: Something went wrong checking for CheckCoreIsolationState!");
+                Console.ResetColor();
+            }
+        }
+
+        public static void CheckForWindowsBuild()
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+            var _CurrentBuild = registryKey.GetValue("CurrentBuild").ToString();
+            if (int.Parse(_CurrentBuild) < 19000 || int.Parse(_CurrentBuild) > 22000)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"[WindowsBuild]: Your window version is NOT compatible with VRGreen. [{_CurrentBuild}]");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[WindowsBuild]: Your window version is compatible with VRGreen. [{_CurrentBuild}]");
+                Console.ResetColor();
+            }
+        }
+
+        public static void CheckCompatibility()
+        {
+            string[] apps = new string[] { "medal", "faceit" }; // I do this incase we find another program that is not compatible.
+
+            foreach (string appName in apps)
+            {
+                Process[] p = Process.GetProcessesByName(appName);
+                if (p.Length == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"[CompatibilityChecker]: {appName} is not running!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[CompatibilityChecker]: {appName} is running!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+    }
+}
---
+++ VRGreen-Troubleshooter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace VRGreen_Troubleshooter
+{
+    internal class Program
+    {
+        // Check Real Time Protection
+        // Check Core Isolation
+        // Check Medal 
+        // Check Windows Build
+        // Check Anti Cheats (FACEIT)
+        // Check Anti Viruses
+        static void Main(string[] args)
+        {
+            Console.Title = "VRGreen Troubleshooter by Sypherr#0001";
+
+            Helpers.CheckRealTimeProtectionState();
+            Helpers.CheckCoreIsolationState();
+            Helpers.CheckForWindowsBuild();
+            Helpers.CheckCompatibility();
+            Helpers.CheckForAntiVirus();
+            Console.ReadKey();
+        }
+    }
+}
